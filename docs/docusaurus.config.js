3da6d148d41c8227a80c3ea2078a1c7d85c40df2
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2024-08-04 23:16:43 +0200

    set baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,12 +16,12 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/AURCache',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lukas-heiligenbrunner', // Usually your GitHub org/user name.
-  projectName: 'aurcache.github.io',
+  projectName: 'lukas-heiligenbrunner.github.io',
   trailingSlash: false,
 
   onBrokenLinks: 'warn',
