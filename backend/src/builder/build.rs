e4cc318f0dd3b29172d7c908b3225b72566c53c6
Lukas-Heiligenbrunner <30468956+Lukas-Heiligenbrunner@users.noreply.github.com>
2024-08-18 22:26:37 +0200

    Build job timeout (#43)
    * add build timeout to build jobs

--- backend/src/builder/build.rs
+++ backend/src/builder/build.rs
@@ -13,15 +13,16 @@ use sea_orm::{
 };
 use shiplift::rep::ContainerCreateInfo;
 use shiplift::tty::TtyChunk;
-use shiplift::{ContainerOptions, Docker, LogsOptions, PullOptions};
+use shiplift::{Container, ContainerOptions, Docker, LogsOptions, PullOptions};
 use std::collections::HashMap;
 use std::fs::Permissions;
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 use std::sync::Arc;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::{env, fs};
 use tokio::sync::Mutex;
+use tokio::time::timeout;
 
 pub(crate) async fn cancel_build(
     build_id: i32,
@@ -144,17 +145,33 @@ pub async fn build(
     repull_image(&docker, &build_logger).await?;
 
     let (create_info, host_active_build_path) =
-        create_build_container(&docker, build_id, name).await?;
+        create_build_container(&docker, build_id, name.clone()).await?;
     let id = create_info.id;
 
+    let container = docker.containers().get(&id);
+
     // start build container
-    docker.containers().get(&id).start().await?;
+    container.start().await?;
 
     // insert container id to container map
     job_containers.lock().await.insert(build_id, id.clone());
 
     // monitor build output
-    monitor_build_output(&build_logger, &docker, id).await?;
+    match timeout(
+        job_timeout_from_env(),
+        monitor_build_output(&build_logger, &container),
+    )
+    .await
+    {
+        Ok(v) => v?,
+        Err(_) => {
+            build_logger
+                .append(format!("Build #{build_id} timed out for package '{name}'"))
+                .await?;
+            // kill build container
+            container.kill(Some("SIGKILL")).await?;
+        }
+    }
 
     // remove build from container map
     job_containers
@@ -174,10 +191,9 @@ pub async fn build(
 
 async fn monitor_build_output(
     build_logger: &BuildLogger,
-    docker: &Docker,
-    id: String,
+    container: &Container<'_>,
 ) -> anyhow::Result<()> {
-    let mut logs_stream = docker.containers().get(id).logs(
+    let mut logs_stream = container.logs(
         &LogsOptions::builder()
             .follow(true)
             .stdout(true)
@@ -277,6 +293,16 @@ fn create_build_paths(name: String) -> anyhow::Result<(String, PathBuf)> {
     let host_build_path_docker =
         env::var("BUILD_ARTIFACT_DIR").unwrap_or(host_build_path_base.display().to_string());
     Ok((host_build_path_docker, host_active_build_path))
+}
+
+fn job_timeout_from_env() -> Duration {
+    let job_timeout = env::var("JOB_TIMEOUT")
+        .ok()
+        .and_then(|x| x.parse::<u64>().ok())
+        .map(Duration::from_secs)
+        .unwrap_or(Duration::from_secs(60 * 60));
+    debug!("job_timeout: {} sec", job_timeout.as_secs());
+    job_timeout
 }
 
 fn limits_from_env() -> (u64, i64) {
