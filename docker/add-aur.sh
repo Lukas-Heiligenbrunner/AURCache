e2453c6cdd3fe05f37332058b099cd14d8a07005
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2024-08-30 20:07:43 +0200

    use prebuilt paru

--- docker/add-aur.sh
+++ docker/add-aur.sh
@@ -7,7 +7,6 @@ set -o verbose
 set -o xtrace
 
 AUR_USER="${1:-ab}"
-HELPER="${2:-yay}"
 
 # update mirrorlist
 #curl --silent --location https://raw.githubusercontent.com/greyltc/docker-archlinux/master/get-new-mirrors.sh > /tmp/get-new-mirrors
@@ -61,17 +60,17 @@ mkdir -p "${FPP}"
 install -o "${AUR_USER}" -d "${FOREIGN_PKG}"
 
 # get helper pkgbuild
-sudo -u "${AUR_USER}" -D~ bash -c "curl --silent --location https://aur.archlinux.org/cgit/aur.git/snapshot/${HELPER}.tar.gz | bsdtar -xvf -"
+sudo -u "${AUR_USER}" -D~ bash -c "curl --silent --location https://aur.archlinux.org/cgit/aur.git/snapshot/paru-bin.tar.gz | bsdtar -xvf -"
 
 # make helper
-sudo -u "${AUR_USER}" -D~//${HELPER} bash -c "makepkg -s --noprogressbar --noconfirm --needed"
+sudo -u "${AUR_USER}" -D~//paru-bin bash -c "makepkg -s --noprogressbar --noconfirm --needed"
 
 # install helper
 pacman --upgrade --needed --noconfirm --noprogressbar "${NEW_PKGDEST}"/*.pkg.*
 
 # cleanup
 sudo rm -rf "${NEW_PKGDEST}"/*
-rm -rf "${AUR_USER_HOME}/${HELPER}"
+rm -rf "${AUR_USER_HOME}/paru-bin"
 rm -rf "${AUR_USER_HOME}/.cache/go-build"
 rm -rf "${AUR_USER_HOME}/.cargo"
 
