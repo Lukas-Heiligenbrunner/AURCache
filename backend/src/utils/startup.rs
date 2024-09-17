2822ad7b5d24640a225c5fbdc602d6d415874bea
lukas-heilgenbrunner <lukas.heiligenbrunner@gmail.com>
2024-09-17 11:36:35 +0200

    fix linting

--- backend/src/utils/startup.rs
+++ backend/src/utils/startup.rs
@@ -1,6 +1,9 @@
-use log::{error, info, warn};
+use log::{error, info};
 use std::path::PathBuf;
 use tokio::fs;
+
+#[cfg(debug_assertions)]
+use log::warn;
 
 const CONTAINER_STORAGE_DIRS: [&str; 2] = ["/run/containers/storage", "/run/libpod"];
 const START_BANNER: &str = r"
@@ -15,7 +18,11 @@ const START_BANNER: &str = r"
 pub async fn startup_tasks() {
     info!("{}", START_BANNER);
     let latest_commit_sha = option_env!("LATEST_COMMIT_SHA").unwrap_or("dev");
-    info!("Version: {}#{}", env!("CARGO_PKG_VERSION"), latest_commit_sha);
+    info!(
+        "Version: {}#{}",
+        env!("CARGO_PKG_VERSION"),
+        latest_commit_sha
+    );
 
     #[cfg(debug_assertions)]
     warn!("This is a dev build! Consider using a stable release.");
