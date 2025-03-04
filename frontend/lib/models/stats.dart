70bd1486cba93bb347b832add0b0c71f961057b6
Lukas-Heiligenbrunner <30468956+Lukas-Heiligenbrunner@users.noreply.github.com>
2025-03-04 14:16:45 +0100

    Finish dashboard header stats (#155)
    * add average build time trend
    
    * just add trend to total builds and avg build time

--- frontend/lib/models/stats.dart
+++ frontend/lib/models/stats.dart
@@ -5,11 +5,7 @@ class Stats {
       repo_size,
       total_packages;
   final Duration avg_build_time;
-  final double total_build_trend,
-      total_packages_trend,
-      repo_size_trend,
-      avg_build_time_trend,
-      build_success_trend;
+  final double total_build_trend, avg_build_time_trend;
 
   factory Stats.fromJson(Map<String, dynamic> json) {
     return Stats(
@@ -20,24 +16,17 @@ class Stats {
       repo_size: json["repo_size"] as int,
       total_packages: json["total_packages"] as int,
       total_build_trend: json["total_build_trend"] as double,
-      total_packages_trend: json["total_packages_trend"] as double,
-      repo_size_trend: json["repo_size_trend"] as double,
       avg_build_time_trend: json["avg_build_time_trend"] as double,
-      build_success_trend: json["build_success_trend"] as double,
     );
   }
 
-  Stats({
-    required this.total_builds,
-    required this.successful_builds,
-    required this.failed_builds,
-    required this.avg_build_time,
-    required this.repo_size,
-    required this.total_packages,
-    required this.total_build_trend,
-    required this.total_packages_trend,
-    required this.repo_size_trend,
-    required this.avg_build_time_trend,
-    required this.build_success_trend,
-  });
+  Stats(
+      {required this.total_builds,
+      required this.successful_builds,
+      required this.failed_builds,
+      required this.avg_build_time,
+      required this.repo_size,
+      required this.total_packages,
+      required this.total_build_trend,
+      required this.avg_build_time_trend});
 }
