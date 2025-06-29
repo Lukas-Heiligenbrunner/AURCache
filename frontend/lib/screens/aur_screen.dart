7a6e2b5d66c583c70846669b6adbf1e222ecf2c6
Alexandre Bury <alexandre.bury@gmail.com>
2025-06-28 20:57:54 -0400

    Try to query AUR with <3 char queries

--- frontend/lib/screens/aur_screen.dart
+++ frontend/lib/screens/aur_screen.dart
@@ -93,14 +93,16 @@ class _AurScreenState extends State<AurScreen> {
                       ),
                     ),
                     onData: (data) => query.length < 3
-                        ? Column(
-                            children: [
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Text("Type to search for an AUR package")
-                            ],
-                          )
+                        ? data.length > 0
+                            ? AurSearchTable(data: data)
+                            : Column(
+                                children: [
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text("Type to search for an AUR package")
+                                ],
+                              )
                         : AurSearchTable(data: data),
                     provider: getAurPackagesProvider(query),
                   ),
