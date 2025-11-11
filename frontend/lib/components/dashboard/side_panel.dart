0a4f669ba645c3a963a8ce825e5a7bb36cb94136
Lukas Heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2025-11-11 15:53:16 +0100

    add view all button to dashboard to redirect to full-screen activities page

--- frontend/lib/components/dashboard/side_panel.dart
+++ frontend/lib/components/dashboard/side_panel.dart
@@ -3,6 +3,7 @@ import 'package:aurcache/components/build_line_chart.dart';
 import 'package:aurcache/components/dashboard/tile_container.dart';
 import 'package:aurcache/utils/responsive.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../constants/color_constants.dart';
 
@@ -15,12 +16,37 @@ class SidePanel extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 12),
-            child: const Text(
-              "Recent Activity",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: const Text(
+                  "Recent Activity",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              if (context.desktop)
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: secondaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: defaultPadding,
+                      vertical: defaultPadding / (context.mobile ? 2 : 1),
+                    ),
+                  ),
+                  onPressed: () {
+                    context.push("/activities");
+                  },
+                  child: const Text(
+                    "View All",
+                    style: TextStyle(color: Colors.white54),
+                  ),
+                ),
+            ],
           ),
           const SizedBox(height: defaultPadding),
           Responsive(
